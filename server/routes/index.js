f72ca5bd959fbc87068328b77b4a615aa7524e12
Cody Deckard <cjdeckard@gmail.com>
2017-10-24 21:35:42 +0000

    Basic Trade Routes
    Creates basic trade routes. Creates a route to get all requests which
    are not finalized, creates a route to create a new trade, creates a
    route to delete a trade and creates a route to approve a trade.

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,7 @@ const path = require('path')
 
 const users = require('./user')
 const books = require('./books')
+const trades = require('./trades')
 
 /**
  * Expose
@@ -11,6 +12,7 @@ const books = require('./books')
 module.exports = app => {
 	app.use('/api/v1/users', users)
 	app.use('/api/v1/books', books)
+	app.use('/api/v1/trades', trades)
 	app.use(express.static(path.resolve(__dirname, '../../', 'client', 'build')))
 	app.get('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, '../../client/build/', 'index.html'))
