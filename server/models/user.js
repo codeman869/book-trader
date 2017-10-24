5767e33fa34198d692d93d56f7723712857b069e
Cody Deckard <cjdeckard@gmail.com>
2017-10-24 20:24:28 +0000

    Creates New Trade Model
    Creates a new trade model which will be used to track trades in the
    system. Each trade has a finalized boolean and is realted to exactly
    one owner and one recepient. Additionally, each trade is also linked
    to a book (item) to be traded.

--- server/models/user.js
+++ server/models/user.js
@@ -3,6 +3,7 @@ const bCrypt = require('bcrypt-nodejs')
 
 const db = require('../db')
 const Book = require('./book')
+const Trade = require('./trade')
 
 function createHash(password) {
 	let hash = bCrypt.hashSync(password, bCrypt.genSaltSync(10), null)
@@ -51,6 +52,11 @@ User.prototype.validPassword = function(password) {
 //Many to Many Relationship
 User.belongsToMany(Book, { as: 'books', through: 'book_owners' })
 Book.belongsToMany(User, { as: 'owners', through: 'book_owners' })
+
+//One to one relationships
+Trade.belongsTo(User, { as: 'owner' })
+Trade.belongsTo(User, { as: 'receiver' })
+Trade.belongsTo(Book, { as: 'item' })
 
 /**
  * Expose
